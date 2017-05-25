5b2105581ff6adc08706fa466cde637c4f614ba1
sheik <sheik.abdullah@inapp.com>
2017-05-25 17:04:47 +0530

    admin login controller

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    //
+
+
+    public function index()
+    {
+    	return view('admin.login');
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Second Route method – Root URL with ID will match this method
+Route::get('ID/{id}',function($id){
+   echo 'ID: '.$id;
+});
+
+// Third Route method – Root URL with or without name will match this method
+Route::get('/user/{name?}',function($name = 'Virat Gandhi'){
+   echo "Name: ".$name;
+});
+
+//Route to admin login page
+Route::get('/admin/login', 'Admin\LoginController@index');
